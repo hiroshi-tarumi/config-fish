0053c3137065d70248293a2378db6d4bc8106452
hiroshi-tarumi <tarumi8171@gmail.com>
2022-09-08 10:09:17 +0900

    Update fish_prompt_get_left_prompt.fish

--- functions/fish_prompt_get_left_prompt.fish
+++ functions/fish_prompt_get_left_prompt.fish
@@ -31,4 +31,22 @@ function fish_prompt_get_left_prompt --description 'Get first line left prompt c
         printf ''
     end
     set_color normal
+
+        # if git repository
+    if git rev-parse --git-dir > /dev/null 2>&1
+        set -l cl_gorse 'FFEB3B'
+        set -l cl_empress '757575'
+        set -l cl_black '000000'
+        set_color $cl_gorse -b normal
+        printf ''
+        set_color $cl_black -b $cl_gorse
+        printf '  '
+        set_color $cl_empress -b $cl_gorse
+        printf ''
+        set_color normal -b $cl_empress
+        printf ' %s' (fish_git_prompt '%s')
+        set_color normal
+    else
+        printf ' '
+    end
 end
