6b0340ee7e31d74766e260ae99bec4b80c563e1c
hiroshi-tarumi <tarumi8171@gmail.com>
2022-09-08 20:02:00 +0900

    git status new create

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,29 +1,51 @@
 function fish_prompt --description 'Write out the prompt'
-    set last_status $status
+    set -l laststatus $status
 
-    # get prompts for the first line
-    set -l left_prompt (fish_prompt_get_left_prompt)
-    set -l right_prompt (fish_prompt_get_right_prompt)
+    set -l git_info
+    if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+        set git_branch (set_color -o blue)"$git_branch"
+        set -l git_status
+        if not command git diff-index --quiet HEAD --
+            if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
+                echo $count | read -l ahead behind
+                if test "$ahead" -gt 0
+                    set git_status "$git_status"(set_color red)⬆
+                end
+                if test "$behind" -gt 0
+                    set git_status "$git_status"(set_color red)⬇
+                end
+            end
+            for i in (git status --porcelain | string sub -l 2 | sort | uniq)
+                switch $i
+                    case "."
+                        set git_status "$git_status"(set_color green)✚
+                    case " D"
+                        set git_status "$git_status"(set_color red)✖
+                    case "*M*"
+                        set git_status "$git_status"(set_color green)✱
+                    case "*R*"
+                        set git_status "$git_status"(set_color purple)➜
+                    case "*U*"
+                        set git_status "$git_status"(set_color brown)═
+                    case "??"
+                        set git_status "$git_status"(set_color red)≠
+                end
+            end
+        else
+            set git_status (set_color green):
+        end
+        set git_info "(git$git_status$git_branch"(set_color white)")"
+    end
 
-    # spaces
-    set -l left_length (fish_prompt_visual_length $left_prompt)
-    set -l right_length (fish_prompt_visual_length $right_prompt)
-    set -l spaces (math "$COLUMNS - $left_length - $right_length")
+    # Disable PWD shortening by default.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
 
-    # Display first line
-    printf '\n'
-    printf '%s' $left_prompt
-    set_color -b normal
-    printf "%-"$spaces"s" " "
-    printf '%s' $right_prompt
-    set_color normal
-
-    # Display second line
-    set -l color_cursor '8829F9'
-    printf '\n'
-    set_color white -b normal
-    printf ' >$ '
-    #set_color $color_cursor -b normal
-    #printf ' '
-    set_color normal
+    set_color -b black
+    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (prompt_pwd) (set_color white) $git_info (set_color white) '❱' (set_color white)
+    if test $laststatus -eq 0
+        printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
+    else
+        printf "%s✘%s≻%s " (set_color -o red) (set_color white) (set_color normal)
+    end
 end
